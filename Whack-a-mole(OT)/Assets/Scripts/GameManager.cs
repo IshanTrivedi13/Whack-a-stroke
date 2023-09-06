348f9dcd7c117f615c6ddc408637efb9d17b3c13
Headhunter0007 <yatharth1207@gmail.com>
2023-09-06 11:22:07 +0530

    Update GameManager.cs

--- Whack-a-mole(OT)/Assets/Scripts/GameManager.cs
+++ Whack-a-mole(OT)/Assets/Scripts/GameManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<Alien> hogs;
+
     private HashSet<Alien> currentHogs = new HashSet<Alien>();
     int prevIndex = 0;
-    private void Start() 
+
+    // Add timer variables
+    private float timer = 30f;
+    private bool isGameActive = true;
+
+    private void Start()
     {
         // Hide all the visible hogs.
-        for (int i = 0; i < hogs.Count; i++) 
+        for (int i = 0; i < hogs.Count; i++)
         {
             hogs[i].Hide();
             hogs[i].SetIndex(i);
@@ -19,25 +25,41 @@ public class GameManager : MonoBehaviour
         currentHogs.Clear();
     }
 
-    public void Update() 
+    public void Update()
     {
-        if (currentHogs.Count <= (0.5)) 
+        if (isGameActive)
         {
-            // Choose a random hog.
-            int index = Random.Range(0, hogs.Count);
-            if (!currentHogs.Contains(hogs[index]) && hogs[index] != hogs[prevIndex]) 
+            // Update the timer
+            timer -= Time.deltaTime;
+
+            // Check if the timer has reached 0
+            if (timer <= 0f)
             {
-                currentHogs.Add(hogs[index]);
-                hogs[index].Activate();
-                prevIndex = index;
+                // Game over logic here
+                isGameActive = false;
+                Debug.Log("Game Over");
+            }
+
+            if (currentHogs.Count <= 0.5)
+            {
+                // Choose a random hog.
+                int index = Random.Range(0, hogs.Count);
+                if (!currentHogs.Contains(hogs[index]) && hogs[index] != hogs[prevIndex])
+                {
+                    currentHogs.Add(hogs[index]);
+                    hogs[index].Activate();
+                    prevIndex = index;
+                }
             }
         }
     }
-    public void Whacked(int hogIndex) 
+
+    public void Whacked(int hogIndex)
     {
         currentHogs.Remove(hogs[hogIndex]);
     }
-    public void Missed(int hogIndex) 
+
+    public void Missed(int hogIndex)
     {
         currentHogs.Remove(hogs[hogIndex]);
     }
