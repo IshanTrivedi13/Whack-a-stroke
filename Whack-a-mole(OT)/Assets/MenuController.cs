753dade8454305cdfc4713c31647c5d3b9008e23
Aakanksha Yadav <Tina95461@gmail.com>
2023-09-06 14:22:45 +0530

    UI_Work

--- Whack-a-mole(OT)/Assets/MenuController.cs
+++ Whack-a-mole(OT)/Assets/MenuController.cs
@@ -8,11 +8,69 @@ public class MenuController : MonoBehaviour
 
     public void Lvl1()
     {
+        
         SceneManager.LoadScene("Level_1");
         Debug.Log("sceneswitch");
     }
-              
-         
 
-    
+    public void Lvl2()
+    {
+        SceneManager.LoadScene("Level_2");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl3()
+    {
+        SceneManager.LoadScene("Level_3");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl34()
+    {
+        SceneManager.LoadScene("Level_4");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl5()
+    {
+        SceneManager.LoadScene("Level_5");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl6()
+    {
+        SceneManager.LoadScene("Level_6");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl7()
+    {
+        SceneManager.LoadScene("Level_3");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl3()
+    {
+        SceneManager.LoadScene("Level_7");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl8()
+    {
+        SceneManager.LoadScene("Level_8");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl9()
+    {
+        SceneManager.LoadScene("Level_9");
+        Debug.Log("sceneswitch");
+    }
+
+    public void Lvl10()
+    {
+        SceneManager.LoadScene("Level_10");
+        Debug.Log("sceneswitch");
+    }
+
 }
