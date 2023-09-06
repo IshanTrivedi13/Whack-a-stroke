c68d969ca663cab9a1493d8e160f4bea177d4de8
Aakanksha Yadav <Tina95461@gmail.com>
2023-09-06 15:34:06 +0530

    UI UPDATE

---
+++ Whack-a-mole(OT)/Assets/SettingScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingScript : MonoBehaviour
+{
+    public void Lvl1()
+    {
+
+        SceneManager.LoadScene("SettingsPage");
+        Debug.Log("sceneswitch");
+    }
+
+}
